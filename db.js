cb7cc25a7fa988adfc64747d612845e658baadc9
rus <kipnis.rus@gmail.com>
2022-01-23 14:05:16 -0500

    added additional features to creating user

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const Pool = require("pg").Pool;
 const bcrypt = require('bcrypt');
+const {emailValidation} = require("./validation");
 
 const pool = new Pool({
     host: `${process.env.DB_HOST}`,
@@ -13,6 +14,23 @@ const pool = new Pool({
 const createUser = async (request, response) => {
     try{
         const {username, firstname, lastname, password, email, country, joindate} = request.body;
+        //validating email
+        if(!emailValidation(email)) {
+            response.status(400).json({ error: "Email not valid" });
+            return;
+        }
+        //checking if theres a duplicate email
+        const emailDuped = await pool.query("SELECT * FROM users WHERE email = $1",[email]);
+        if(emailDuped.rows.length > 0){
+            response.status(400).json({ error: "Email already exists" });
+            return;
+        }
+        const usernameDupe = await pool.query("SELECT * FROM users WHERE username = $1",[username]);
+        if(usernameDupe.rows.length > 0){
+            response.status(400).json({ error: "User already exists" });
+            return;
+        }
+
         const salt = await bcrypt.genSalt(10); // adding layer of security
         const hashedPassword = await bcrypt.hash(password,salt); // hashing the password
         const newUser = await pool.query("INSERT INTO users (username, firstname, lastname, password, email, country, joindate) VALUES ($1, $2, $3, $4, $5, $6, now()) RETURNING *",
---
+++ validation.js
@@ -0,0 +1,9 @@
+const emailValidation = (email) => {
+    const RegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (RegEx.test(email)) {
+      return true;
+    }
+    return false;
+  };
+
+module.exports = {emailValidation};
