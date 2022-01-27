9d7b53c10b744784abe2d57c2ee063f78ee50615
rus <kipnis.rus@gmail.com>
2022-01-27 02:04:18 -0500

    login fix

--- db.js
+++ db.js
@@ -66,10 +66,13 @@ const createUser = async (request, response) => {
 // login user
 const loginUser = async (request, response) => {
   try {
-    const { username, password } = request.body;
-    const user = await pool.query("SELECT * FROM users WHERE username=$1", [
-      username
-    ]);
+    // const { username, password } = request.body;
+    const username = request.body.username;
+    const pass = request.body.password;
+    const login = await pool.query("SELECT * FROM users username=? AND password=? VALUES($1,$2)", [username, password]);
+    // const user = await pool.query("SELECT * FROM users WHERE username=$1", [
+    //   username
+    // ]);
     const match = await bcrypt.compare(password, user.rows[0].password);
     if (!match) {
       response.status(400).json({ error: "Password is incorrect" });
