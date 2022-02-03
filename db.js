84740f51c0d554be570f78ce58a473c61404f6b1
rus <kipnis.rus@gmail.com>
2022-02-03 11:11:28 -0500

    datefix 2

--- db.js
+++ db.js
@@ -126,7 +126,7 @@ const createComment = async (request, response) => {
 const getComment = async (request, response) => {
   try {
     // const comments = await pool.query("SELECT * FROM comments");
-    const comments = await pool.query("SELECT id, description, to_char(date,'YYYY-MM-DD') as date,currentuser from comments");
+    const comments = await pool.query("SELECT id, description, to_date(date,'YYYY-MM-DD') as date,currentuser from comments");
     response.json(comments.rows);
   } catch (err) {
     response.status(500).json({ error: err.message });
